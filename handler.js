e7f09a5f815c1642574b7d10db3f9f6d2f7175c6
j@sharpi.sh <j@sharpi.sh>
2022-11-07 17:40:06 +0800

    minor change to query

--- handler.js
+++ handler.js
@@ -94,34 +94,34 @@ app.get("/inventory", async function (req, res) {
 app.put("/inventory-discount/:category", async function (req, res) {
   const discountValue = 0.8;
 
+  //@todo theres a typo in the category property in postman, its not worth fixing it at this point!
+
+  //if(typeof req.params.category !== 'undefined'){
+      
   const bulkUpdate = async () => {
-    // retrieve the data items in the specified category
-    const { Items = [] } = await dynamoDbClient
-      .scan({
-        TableName: INVENTORY_TABLE,
-        FilterExpression: "begins_with(#id, :id)",
-        ExpressionAttributeNames: {
-          "#id": "id",
-        },
-        ExpressionAttributeValues: {
-          ":id": "user",
-        },
-      })
-      .promise();
-
-    //if(typeof req.params.category !== 'undefined'){
-
-    // update by category
-
-    //@todo theres a typo in the category property in postmna, its not worth fixing it at this point!
+        
+        // retrieve the data items in the specified category
+        const { Items = [] } = await dynamoDbClient
+          .scan({
+            TableName: INVENTORY_TABLE,
+            FilterExpression: "(#cateory, :cateory)",
+            ExpressionAttributeNames: {
+              "#id": "id",
+            },
+            ExpressionAttributeValues: {
+              ":id": "user",
+            },
+          })
+          .promise();
 
     //} else {
 
-    // update all, get alll data
+        // get alll data
 
     //}*/
 
     const bulkUpdatePromises = Items.map(async (item) => {
+      
       // update each item with the private visibility
 
       await documentClient
